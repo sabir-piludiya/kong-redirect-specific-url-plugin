4851ad64be0027059013cbb86e866bee9d0bdfce
Sabir Piludiya <61137209+sabir-piludiya@users.noreply.github.com>
2021-05-17 11:13:07 +0530

    Update schema.lua

--- src/schema.lua
+++ src/schema.lua
@@ -15,6 +15,26 @@ return {
             type     = "string",
             required = true,
             default  = "https://github.com/sabir-piludiya/kong-plugin-redirect-test"
+        },
+        path2 = {
+            type     = "string",
+            required = true,
+            default  = "/"
+        },
+        redirect_url2 = {
+            type     = "string",
+            required = true,
+            default  = "https://github.com/sabir-piludiya/kong-plugin-redirect-test"
+        },
+        path3 = {
+            type     = "string",
+            required = true,
+            default  = "/"
+        },
+        redirect_url3 = {
+            type     = "string",
+            required = true,
+            default  = "https://github.com/sabir-piludiya/kong-plugin-redirect-test"
         }
     }
 }
